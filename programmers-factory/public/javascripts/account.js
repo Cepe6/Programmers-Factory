0abaf913d6621f419166a21b8fe336db1612b279
Cepe6 <vmladenov22@gmail.com>
2017-05-05 08:37:40 +0300

    Make the form closable

--- programmers-factory/public/javascripts/account.js
+++ programmers-factory/public/javascripts/account.js
@@ -5,3 +5,8 @@ function showAccountForm() {
     document.getElementById("overlay").style.display = "block";
     document.getElementById("account-form").style.display = "block";
 }
+
+function closeAccountForm() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("account-form").style.display = "none";
+}
